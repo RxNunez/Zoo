a5a7b3f9d88d8afd4ec7e9475151840dd069c6e9
RxNunez <Nunez.ryan@gmail.com>
2017-09-22 10:40:31 -0700

    include firstSpecies

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Zoo</h1>
+  <div class="container">
+    <h1>Zoo Tracker for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+       <li>{{firstSpecies.description}}</li>
+     </ul>
+  </div>
   `
 })
 
 export class AppComponent {
+  currentFocus: string = 'Zoo Tracker';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+   day: number = this.currentTime.getDate();
+   year: number = this.currentTime.getFullYear();
+   firstSpecies = {
+    description: "Arctic Fox"
+  }
 
 }
