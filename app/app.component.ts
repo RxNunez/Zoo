880ce5e0da716bb900914f690c0202144dc0a140
RxNunez <Nunez.ryan@gmail.com>
2017-09-22 10:56:24 -0700

    include list of initial species

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
     <h1>Zoo Tracker for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
     <ul>
-       <li>{{firstSpecies.description}}</li>
+       <li *ngFor="let currentSpecies of species">{{currentSpecies.description}}</li>
      </ul>
   </div>
   `
@@ -19,8 +19,15 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
    day: number = this.currentTime.getDate();
    year: number = this.currentTime.getFullYear();
-   firstSpecies = {
-    description: "Arctic Fox"
+   Specie: Species[] = [
+   new Species("Arctic Fox"),
+   new Species("Ocelot"),
+   new Species("Northwest Black Tailed Deer")
+   ];
+    description:
   }
+  export class Species {
+  public done: boolean = false;
+  constructor(public description: string) { }
 
 }
